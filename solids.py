54a399e77d8e958c642b97da10856b8d89ee686a
Rafał Mokrzycki <55636265+rafal-mokrzycki@users.noreply.github.com>
2022-11-23 11:37:21 +0100

    Solids (#1)
    * add Ellipsoid
    
    * add Ball
    
    * add RectangularCuboid
    
    * add Cube
    
    * Ref
    
    * Ref

---
+++ solids.py
@@ -0,0 +1,43 @@
+import math
+
+
+class Ellipsoid:
+    """A class of Python object that describes the properties of an Ellipsoid"""
+
+    def __init__(
+        self,
+        semi_axis_1,
+        semi_axis_2,
+        semi_axis_3,
+    ):
+        self.semi_axis_1 = semi_axis_1
+        self.semi_axis_2 = semi_axis_2
+        self.semi_axis_3 = semi_axis_3
+
+    def compute_volume(self):
+        return (4 / 3) * math.pi * self.semi_axis_1 * self.semi_axis_2 * self.semi_axis_3
+
+
+class Ball(Ellipsoid):
+    def __init__(self, radius):
+        super().__init__(radius, radius, radius)
+
+
+class RectangularCuboid:
+    def __init__(
+        self,
+        length,
+        width,
+        height,
+    ):
+        self.width = width
+        self.height = height
+        self.length = length
+
+    def compute_volume(self):
+        return self.width * self.height * self.length
+
+
+class Cube(RectangularCuboid):
+    def __init__(self, width):
+        super().__init__(width, width, width)
