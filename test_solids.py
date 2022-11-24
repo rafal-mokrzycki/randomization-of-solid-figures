7896fd47aeba71d27b8be956a32696bc1bfe7a2d
rafal-mokrzycki <55636265+rafal-mokrzycki@users.noreply.github.com>
2022-11-24 10:21:42 +0100

    add draft tests

---
+++ test_solids.py
@@ -0,0 +1,28 @@
+""" to run: python -m pytest -vv test_solids.py -s
+add --pdb to debug in command line
+use VScode test tools reccomended (the beaker)
+to debug VScode: pytest --collect-only """
+
+
+def test_compute_volume_ball():
+    pass
+
+
+def test_compute_volume_ellipsoid():
+    pass
+
+
+def test_compute_volume_rect_cuboid():
+    pass
+
+
+def test_compute_volume_cube():
+    pass
+
+
+def test_compute_volume_cone():
+    pass
+
+
+def test_compute_volume_cylinder():
+    pass
